8c3a855357b4621cf49808eb9b34ef7485f6b94d
Mihaela špoljarić <mihaela.s27@gmail.com>
2018-04-26 09:30:33 +0200

    dropdown

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,13 +3,11 @@
 	<head>
 		@include('partials._head')
 		<!-- include libraries(jQuery, bootstrap) -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
 	<!-- jQuery library -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<!-- JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link href="https://fonts.googleapis.com/css?family=Bungee+Hairline|Dancing+Script|Indie+Flower|Poiret+One|Shadows+Into+Light" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
